2bd3f8f7592d1e6a24bda0982d4b6c3948a10b98
masaki-yosida <yashukasono@gmail.com>
2023-11-21 12:54:16 +0900

    s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
+  before_action :configure_sign_up_params, only: [:create]
+
+  def create
+    @user = build_resource(sign_up_params)
+
+    if @user.save
+      # 保存成功の処理
+    else
+      Rails.logger.debug "User save failed. Errors: #{user.errors.full_messages}"
+      # 保存失敗時の処理
+    end
+  end
+
 
   private
 
@@ -9,5 +22,11 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
   end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :nickname, :firstname_kanji, :lirstname_kanji, :firstname_kana, :lastname_kana, :birthday])
+  end
 
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :nickname, :firstname_kanji, :lirstname_kanji, :firstname_kana, :lastname_kana, :birthday)
+  end
 end
