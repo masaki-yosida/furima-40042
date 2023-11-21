a64d0a07fc22f5124c4bc04599f805e9a5c6d217
masaki-yosida <yashukasono@gmail.com>
2023-11-21 19:43:11 +0900

    エラーメッセージが表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+
+
+  
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "is invalid. Include both letters and numbers" }
+  validates :firstname_kanji, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters" }
+  validates :lirstname_kanji, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters" }
+  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+
+
 end
