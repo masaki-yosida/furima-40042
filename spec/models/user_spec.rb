6f061b73defce8b0433c9b87dd8818d11573135e
masaki-yosida <yashukasono@gmail.com>
2023-11-21 20:34:33 +0900

    単体テスト済み

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it 'emailが空では登録できない' do
+      user = User.new(nickname: 'test', email: '', password: '000000', password_confirmation: '000000')
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'passwordが空では登録できない' do
+      user = User.new(nickname: 'test', email: 'test@example', password: '', password_confirmation: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Password can't be blank")
+    end
+    it 'birthdayが空では登録できない' do
+      user = User.new(nickname: 'test', email: 'test@example.com', password: 'password', password_confirmation: '', birthday: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Birthday can't be blank")
+    end
+    it 'firstname_kanjiが空では登録できない' do
+      user = User.new(nickname: 'test', email: 'test@example.com', password: 'password', password_confirmation: 'password', birthday: '1990-01-01', firstname_kanji: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Firstname kanji can't be blank")
+    end
+    it 'lirstname_kanjiが空では登録できない' do
+      user = User.new(nickname: 'test', email: 'test@example.com', password: 'password', password_confirmation: 'password', birthday: '1990-01-01', firstname_kanji: '田中', lirstname_kanji: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Lirstname kanji can't be blank")
+    end
+    it 'firstname_kanaが空では登録できない' do
+      user = User.new(nickname: 'test', email: 'test@example.com', password: 'password', password_confirmation: 'password', birthday: '1990-01-01', firstname_kanji: '田中', lirstname_kanji: '家', firstname_kana: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Firstname kana can't be blank")
+    end
+    it 'lastname_kanaが空では登録できない' do
+      user = User.new(nickname: 'test', email: 'test@example.com', password: 'password', password_confirmation: 'password', birthday: '1990-01-01', firstname_kanji: '田中', lirstname_kanji: '家', firstname_kana: 'カナ', lastname_kana: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Lastname kana can't be blank")
+    end
+    
+  end
+end
