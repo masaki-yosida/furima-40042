f7e6d5fe7e95c95bc90483ab4d438d41df3680ef
masaki-yosida <yashukasono@gmail.com>
2023-11-22 12:11:00 +0900

    パスワードとパスワード（確認）は、値の一致が必須であること以外のテストコードは記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,17 @@ class User < ApplicationRecord
   
   validates :email, presence: true
   validates :password, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
   validates :nickname, presence: true
   validates :birthday, presence: true
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "is invalid. Include both letters and numbers" }
-  validates :firstname_kanji, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters" }
-  validates :lirstname_kanji, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters" }
+  validates :firstname_kanji, presence: true, format: { with: /\A[ァ-ヶー－一-龠]+\z/, message: "is invalid. Input full-width characters" }
+  validates :lirstname_kanji, presence: true, format: { with: /\A[ァ-ヶー－一-龠]+\z/, message: "is invalid. Input full-width characters" }
   validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
   validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :email, presence: true, uniqueness: true
+  
 
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,6 +42,27 @@ RSpec.describe User, type: :model do
       user.valid?
       expect(user.errors.full_messages).to include("Lastname kana can't be blank")
     end
-    
+    it 'emailが@を含まない場合は登録できない' do
+      user = User.new(nickname: 'test', email: 'testexample.com', password: 'password', password_confirmation: 'password', birthday: '1990-01-01', firstname_kanji: '田中', lirstname_kanji: '家', firstname_kana: 'カナ', lastname_kana: 'ミョウジ')
+      user.valid?
+      expect(user.errors.full_messages).to include("Email is invalid")
+    end
+    it 'パスワードが6文字以上であれば登録できる' do
+      user = User.new(
+        nickname: 'test',
+        email: 'test@example.com',
+        password: 'Abcd12', # 6文字のパスワード
+        password_confirmation: 'Abcd12',
+        birthday: '1990-01-01',
+        firstname_kanji: '田中',
+        lirstname_kanji: '家',
+        firstname_kana: 'カナ',
+        lastname_kana: 'ミョウジ'
+      )
+      expect(user).to be_valid
+    end
+
   end
 end
+
+
