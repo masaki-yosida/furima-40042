f334876330d6c043e22acd70ea0ff0b6825eceae
masaki-yosida <yashukasono@gmail.com>
2023-11-25 16:39:22 +0900

    Revert "消す"
    This reverts commit 216cd280650ffde8f1c72a6593c7a9860f0beaab.

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,22 @@
-# config/application.rb
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 module Furima40042
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.to_prepare do
-      Devise::RegistrationsController.before_action :configure_sign_up_params, only: [:create]
-    end
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
-
