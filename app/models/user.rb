4c7ce74773cc88a03adeeaf9f560a1999363b185
masaki-yosida <yashukasono@gmail.com>
2023-11-25 18:19:37 +0900

    Revert "消す"
    This reverts commit fc08b7c896566bf7ddeed01f05126968177d9be8.

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,3 @@ class User < ApplicationRecord
   validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
   validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
   validates :email, presence: true, uniqueness: true
-end
