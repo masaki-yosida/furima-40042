d1bb866b5afc0dcabf2450ff0f8780e2a89d196d
masaki-yosida <yashukasono@gmail.com>
2023-11-27 16:14:01 +0900

    Revert "消す"
    This reverts commit 89562398da574cc8cc29225c59d4f9f9e5e39177.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,8 @@
-# spec/factories/users.rb
-
 FactoryBot.define do
   factory :user do
-    nickname              { Faker::Lorem.characters(number: 1) }
-    email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
-    password_confirmation { password }
-    firstname_kanji       { Faker::Japanese::Name.first_name.gsub(/[^一-龯]/, '') }
-    listname_kanji        { Faker::Japanese::Name.list_name.gsub(/[^一-龯]/, '') }
-    firstname_kana        { Faker::Japanese::Name.first_name.yomi.tr("ぁ-ん", "ァ-ン") }
-    lastname_kana         { Faker::Japanese::Name.last_name.yomi.tr("ぁ-ん", "ァ-ン") }
-    birthday              { Faker::Date.birthday.to_s }
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
   end
 end
