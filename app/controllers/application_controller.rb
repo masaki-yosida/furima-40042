3ad278ca0b35278ff0d27abf399a5ecc68260e24
masaki-yosida <yashukasono@gmail.com>
2023-11-28 20:24:59 +0900

    ghjk消す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,19 @@
+# app/controllers/application_controller.rb
+
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
-  before_action :configure_sign_up_params, only: [:create]
-
-  
-
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :nickname, :firstname_kanji, :lirstname_kanji, :firstname_kana, :lastname_kana, :birthday])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :nickname, :firstname_kanji, :lastname_kanji, :firstname_kana, :lastname_kana, :birthday])
   end
-
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
+
 class Item < ApplicationRecord
+  # Ensure that the attribute names match the columns in the database
+  validates :item_name, presence: true
+  validates :item_explanation, presence: true
+  
+
+  has_one_attached :image
+
+  # ... other validations and associations
+
 end
+
+
