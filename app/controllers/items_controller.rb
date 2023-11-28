2a290155d9a9fe0d47c4949ae2ce97cf2e3d22a2
masaki-yosida <yashukasono@gmail.com>
2023-11-28 12:49:44 +0900

    消す

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,10 +14,24 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  # 他のアクションも追加
-  # 例: show, edit, create, update, destroyなど
+  def create
+    # フォームからのデータを使って新しいアイテムを作成
+    @item = Item.new(item_params)
+
+    # 保存が成功した場合と失敗した場合で処理を分岐
+    if @item.save
+      # 保存に成功した場合の処理
+      redirect_to @item, notice: 'アイテムが正常に作成されました。'
+    else
+      # 保存に失敗した場合の処理
+      render :new
+    end
+  end
 
   private
 
-  # 必要に応じてプライベートメソッドを追加
+  def item_params
+    # フォームから受け取るパラメータを指定
+    params.require(:item).permit(:name, :description, :category, :other_attribute) # 実際のカラム名に置き換える
+  end
 end
