97fedc9c615d1e0aa748f8a9f6318b5c2e8dcd51
masaki-yosida <yashukasono@gmail.com>
2023-11-28 12:00:34 +0900

    出品を押したら出品に移行できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,58 +1,23 @@
-# app/controllers/items_controller.rb
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_user, only: [:new, :create]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:restricted_action]
 
   def index
     @items = Item.all
   end
 
-  def show
+  def restricted_action
+    # restricted_action のコードを追加
+    # 例: 特定の処理を実行するなど
   end
 
   def new
-    @item = @user.items.build
+    @item = Item.new
   end
 
-  def create
-    @item = @user.items.build(item_params)
-
-    if @item.save
-      redirect_to user_item_path(@user, @item), notice: 'Item was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @item.update(item_params)
-      redirect_to user_item_path(@user, @item), notice: 'Item was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @item.destroy
-    redirect_to user_items_path(@user), notice: 'Item was successfully destroyed.'
-  end
+  # 他のアクションも追加
+  # 例: show, edit, create, update, destroyなど
 
   private
 
-  def set_user
-    @user = User.find(params[:user_id])
-  end
-
-  def set_item
-    @item = @user.items.find(params[:id])
-  end
-
-  def item_params
-    params.require(:item).permit(:name, :description)
-  end
+  # 必要に応じてプライベートメソッドを追加
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
+  get '/items/new', to: 'items#new', as: 'new_item'
 
   resources :users do
     resources :items
