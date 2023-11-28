32259545e040c1fba895579e3c27ed277d10d06f
masaki-yosida <yashukasono@gmail.com>
2023-11-29 00:53:29 +0900

    出品したらトップに戻る

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,16 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+
   def new
     @item = Item.new
   end
+
   def create
     @item = current_user.items.build(item_params)
 
     if @item.save
-      # Handle successful creation, e.g., redirect to the item's show page
-      redirect_to @item, notice: 'Item was successfully created.'
+      redirect_to root_path, notice: 'Item was successfully created.'
     else
-      # Handle validation errors, e.g., re-render the 'new' form
       render 'new'
     end
   end
@@ -19,9 +19,7 @@ class ItemsController < ApplicationController
 
   private
 
-  
   def item_params
     params.require(:item).permit(:image, :item_name, :item_explanation, :category_id, :situation_id, :postage_id, :prefecture_id, :deliveryday_id, :price)
   end
-  
 end
