4fc6f8b6f00c884e73b7ab5bae8d6350867dbc68
masaki-yosida <yashukasono@gmail.com>
2023-11-29 17:06:56 +0900

    Revert "消す"
    This reverts commit 89bbae6ec32a0245f39df8230dea770badabfbbb.

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,9 +1,5 @@
 FactoryBot.define do
   factory :article do
-    # Add content for the article factory here if needed
-  end
-
-  factory :item do 
-    # Add content for the user factory here
+    
   end
 end
