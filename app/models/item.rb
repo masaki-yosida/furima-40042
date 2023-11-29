026f1c123736a5e9b55cd1baa08901db9fa84fd7
masaki-yosida <yashukasono@gmail.com>
2023-11-29 10:52:25 +0900

    情報が必ず必要の制約追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,27 @@
 class Item < ApplicationRecord
   belongs_to :user
-  validates :item_name, :item_explanation, :category_id, :situation_id, :postage_id, :prefecture_id, :deliveryday_id, :price, presence: true
+
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :item_explanation, presence: true
+  validates :category_id, presence: true
+  validates :situation_id, presence: true
+  validates :postage_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :deliveryday_id, presence: true
+  validates :price, presence: true
+
+
+
+
+
+
+
+
+
+
+
+
   has_one_attached:image
 end
