a37c68b299a73a6ad9b90d376dd6419a650a99a6
masaki-yosida <yashukasono@gmail.com>
2023-11-29 14:55:08 +0900

    バリデーション終了

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,12 +5,12 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :item_name, presence: true
   validates :item_explanation, presence: true
-  validates :category_id, presence: true
-  validates :situation_id, presence: true
-  validates :postage_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :deliveryday_id, presence: true
-  validates :price, presence: true
-  
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :situation_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :deliveryday_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
   has_one_attached:image
 end
