a7c038b0ec5a019738aa9129cb9e64be8cc44bb7
masaki-yosida <yashukasono@gmail.com>
2023-12-01 11:44:49 +0900

    フェイカーの設定終了

---
+++ spec/factories/items.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :item do
+    item_name { Faker::Commerce.product_name }
+    item_explanation { Faker::Lorem.paragraph }
+    image { nil }
+    category_id { Faker::Number.between(from: 2, to: 11) }
+    situation_id { Faker::Number.between(from: 2, to: 7) }
+    postage_id { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    deliveryday_id { Faker::Number.between(from: 2, to: 4) }
+    price { Faker::Commerce.price(range: 100..10000) }
+
+    # Add other attributes as needed
+  end
+end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,34 +4,16 @@ RSpec.describe Item, type: :model do
   describe '異常系' do
     context '商品画像' do
       it '商品画像が空では保存できない' do
-        item = Item.new(
-          item_name: '',
-          item_explanation: 'This is a sample product description.',
-          image: ``,
-          category_id: Category.create(name: 'Some Category').id,
-          situation_id: Situation.create(name: 'Some Situation').id,
-          postage_id: Postage.create(name: '送料込み(出品者負担)').id,
-          prefecture_id: Prefecture.create(name: 'Tokyo').id,
-          deliveryday_id: Deliveryday.create(name: '2 days').id,
-          price: 1000
-        )
+        item = FactoryBot.build(:item)  # Userのインスタンス生成
+        item.nickname = ''  # nicknameの値を空にす
         item.valid?
         expect(item.errors.full_messages).to include "User must exist", "Image can't be blank", "Item name can't be blank"
       end
     end
     context '商品名' do
       it '商品名が空では保存できない' do
-        item = Item.new(
-          item_name: '',
-          item_explanation: 'This is a sample product description.',
-          image: nil,
-          category_id: Category.create(name: 'Some Category').id,
-          situation_id: Situation.create(name: 'Some Situation').id,
-          postage_id: Postage.create(name: '送料込み(出品者負担)').id,
-          prefecture_id: Prefecture.create(name: 'Tokyo').id,
-          deliveryday_id: Deliveryday.create(name: '2 days').id,
-          price: 1000
-        )
+        item = FactoryBot.build(:item)  
+        item.item_name = ''  
         item.valid?
         expect(item.errors.full_messages).to include "User must exist", "Image can't be blank", "Item name can't be blank"
       end
