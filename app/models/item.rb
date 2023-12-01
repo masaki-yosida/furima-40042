2aa2f241b5362ee89de7a1b13316c50086b81bd5
masaki-yosida <yashukasono@gmail.com>
2023-12-01 15:15:11 +0900

    各アクティブハッシュのクラスとのアソシエーション記載場所変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :deliveryday
 
 
+
+
+  validates :title, :text, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :situation_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :deliveryday_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :image, presence: true
   validates :item_name, presence: true
   validates :item_explanation, presence: true
