5e28dcc29c7aac2d23c774aa03b9fcff1ee0afb1
masaki-yosida <yashukasono@gmail.com>
2023-12-01 13:57:11 +0900

    完成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,8 +4,46 @@ RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
   end
+  describe '正常系' do
+    it '全ての項目が入力されていれば登録できる' do
+      # Set values to make the item valid
+      @item.item_name = 'Valid Name'
+      @item.item_explanation = 'Valid Explanation'
+      @item.category_id = 5
+      @item.situation_id = 2
+      @item.postage_id = 3
+      @item.prefecture_id = 4
+      @item.deliveryday_id = 5
+      @item.price = 350
+
+      # Ensure there's a user associated with the item
+      user = FactoryBot.create(:user)
+      @item.user = user
+
+      # Attach a fake image from the fixtures directory
+      image_path = Rails.root.join('spec', 'fixtures', 'test_image.jpg')
+      @item.image.attach(io: File.open(image_path), filename: 'test_image.jpg', content_type: 'image/jpg')
+
+      # Output errors for debugging purposes
+      puts @item.errors.full_messages
+
+      # Expect that the @item instance is valid
+      expect(@item).to be_valid
+    end
+  end
+
+
+
+
 
   describe '異常系' do
+    context '商品画像' do
+      it '商品画像が空では保存できない' do
+        item = FactoryBot.build(:item, image: nil)  # Set image to nil to test the validation
+        item.valid?
+        expect(item.errors.full_messages).to include("User must exist", "Image can't be blank", "Price must be an integer")
+      end
+    end
     context '商品名' do
       it '商品名が空では保存できない' do
         @item.item_name = ''  
