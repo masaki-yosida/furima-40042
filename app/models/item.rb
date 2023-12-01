ba705504c1c3b42324ee94368bf414c05b465cc8
masaki-yosida <yashukasono@gmail.com>
2023-12-01 12:45:43 +0900

    消す

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,6 +11,5 @@ class Item < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :deliveryday_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-
   has_one_attached:image
 end
