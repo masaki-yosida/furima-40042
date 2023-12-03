ac6ec2968b3c6f104cfe210f6355a3359ae7e531
masaki-yosida <yashukasono@gmail.com>
2023-12-03 23:24:28 +0900

    Revert "各アクティブハッシュのクラスとのアソシエーション記載場所変更"
    This reverts commit 2aa2f241b5362ee89de7a1b13316c50086b81bd5.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,16 +1,19 @@
-class Item < ApplicationRecord
-  belongs_to :user
+class Article < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :deliveryday
 
 
-  validates :image, presence: true
-  validates :item_name, presence: true
-  validates :item_explanation, presence: true
+
+
+  validates :title, :text, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :situation_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :deliveryday_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
-  has_one_attached:image
 end
