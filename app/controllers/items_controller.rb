beb359f6f211a24ea72277b4a0f4316f39150866
masaki-yosida <yashukasono@gmail.com>
2023-12-03 23:05:28 +0900

    Revert "消す"
    This reverts commit 41e60e49392836561f2d23708c057205beb08a8d.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,35 +3,27 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    @categories = Category.all
+  end
+def create
+  @item = current_user.items.build(item_params)
+
+  if @item.save
+    redirect_to root_path, notice: 'Item was successfully created.'
+  else
+    render :new, status: :unprocessable_entity
+    @categories = Category.all  # カテゴリーなどの選択肢を再度取得
     @situations = Situation.all
     @postages = Postage.all
     @prefectures = Prefecture.all
     @deliverydays = Deliveryday.all
   end
-
-  def create
-    @item = current_user.items.build(item_params)
-
-    if @item.save
-      redirect_to root_path, notice: 'Item was successfully created.'
-    else
-      # If the save fails, populate the instance variables for the form
-      @categories = Category.all
-      @situations = Situation.all
-      @postages = Postage.all
-      @prefectures = Prefecture.all
-      @deliverydays = Deliveryday.all
-
-      render :new, status: :unprocessable_entity
-    end
-  end
+end
 
   # Other methods...
 
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :item_explanation, :category_id, :situation_id, :postage_id, :prefecture_id, :deliveryday_id, :price, :image)
+    params.require(:item).permit(:image, :item_name, :item_explanation, :category_id, :situation_id, :postage_id, :prefecture_id, :deliveryday_id, :price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,29 @@
 class Item < ApplicationRecord
-  belongs_to :category, class_name: 'Category'
   belongs_to :user
+  belongs_to :category
   belongs_to :situation
   belongs_to :postage
   belongs_to :prefecture
   belongs_to :deliveryday
 
+
+
+
+  validates :title, :text, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :situation_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :deliveryday_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :image, presence: true
   validates :item_name, presence: true
   validates :item_explanation, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :situation_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :postage_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :deliveryday_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :situation_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :deliveryday_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  validates :image, presence: true
 
-  has_one_attached :image
+  has_one_attached:image
 end
