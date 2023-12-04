d12ea628495b5822efb51f579956ca574892e95b
masaki-yosida <yashukasono@gmail.com>
2023-12-04 10:34:38 +0900

    商品価格に関しては、以下を確認するテストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -120,6 +120,21 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Price is not a number"
       end
+      it '半角数字以外の値が含まれている場合は保存できない' do
+        @item.price = 'abc123'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+      it '¥300未満は保存できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
+      end
+      it '10,000,000以上の値では保存できない' do
+        @item.price = 10_000_001
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
+      end
     end
   end
 end
