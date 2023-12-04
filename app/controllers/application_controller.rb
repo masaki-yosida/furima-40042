f69af0ef9720006408eba920cc8562acece0fae7
masaki-yosida <yashukasono@gmail.com>
2023-12-04 14:55:23 +0900

    多分完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :set_active_storage_url_options  # Add this line
   before_action :configure_sign_up_params, only: [:create], if: :devise_controller?
 
   private
@@ -14,5 +15,11 @@ class ApplicationController < ActionController::Base
     if controller_name == 'registrations' && action_name == 'create'
       devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :nickname, :firstname_kanji, :lirstname_kanji, :firstname_kana, :lastname_kana, :birthday])
     end
+  end
+
+  def set_active_storage_url_options
+    ActiveStorage::Current.url_options = {
+      host: request.base_url
+    }
   end
 end
