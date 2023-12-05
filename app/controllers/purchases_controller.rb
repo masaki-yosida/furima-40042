7407546abdba3127d4f37ea6892dc91a5e3e5020
masaki-yosida <yashukasono@gmail.com>
2023-12-05 16:53:53 +0900

    消す

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,7 @@
+class PurchasesController < ApplicationController
+  def new
+    # Your new action logic goes here
+  end
+
+  # Other actions if needed
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
 resources :items
 
+resources :purchases, only: [:index, :new]
+
+
+
 end
