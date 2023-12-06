920162aa89309094b5f4fd503cc29719d76da42a
masaki-yosida <yashukasono@gmail.com>
2023-12-06 17:06:54 +0900

    消す

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,20 @@ class OrdersController < ApplicationController
   def index
     @item = Item.last
   end
-end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price, :token, :postal_code, :prefecture, :city, :addresses, :building, :phone_number)
+  end
+end  
