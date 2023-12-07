a14a6ed99977002bac714411830692c3f5e5f5e1
masaki-yosida <yashukasono@gmail.com>
2023-12-07 16:36:30 +0900

    Revert "消す"
    This reverts commit c503b47b0f573aa280231dfe8a589bd2ec2ef410.

--- app/models/purchase.rb
+++
@@ -1,8 +0,0 @@
-# app/models/purchase.rb
-
-class Purchase < ApplicationRecord
-  belongs_to :user
-  belongs_to :item
-  has_one :shipping_address
-  accepts_nested_attributes_for :shipping_address
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
 
 resources :items
 resources :orders
-resources :purchases
 
 end
