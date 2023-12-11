18c11d336f2b26803641a35299df11b774e75b8c
masaki-yosida <yashukasono@gmail.com>
2023-12-11 18:29:17 +0900

    基本は完成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @item = Item.last
+    @item = Item.find(params[:item_id])
   end
 
   def create
@@ -15,7 +15,6 @@ class OrdersController < ApplicationController
 
       if shipping.save
         pay_item
-        @order = Order.create(purchase: purchase, shipping: shipping)
         redirect_to root_path, notice: '購入が完了しました。'
       else
         render :index
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-
-resources :items
-resources :orders
+  resources :items do
+    resources :orders, only: [:create]
+  end
+resources :items do
+  resources :orders, only: :index
+end
 
 end
