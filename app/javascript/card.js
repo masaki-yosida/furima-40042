43a67375c75da37477759440dd270353cf8fb373
masaki-yosida <yashukasono@gmail.com>
2023-12-11 19:37:54 +0900

    消す

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_110e141f79fd1c929a961ea3')// PAY.JPテスト公開鍵
+  const payjp = Payjp('pk_test_110e141f79fd1c929a961ea3'); // PAY.JPテスト公開鍵
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -8,23 +8,30 @@ const pay = () => {
   numberElement.mount('#number-form');
   expiryElement.mount('#expiry-form');
   cvcElement.mount('#cvc-form');
-  const form = document.getElementById('charge-form')
-  form.addEventListener("submit", (e) => {
-    payjp.createToken(numberElement).then(function (response) {
-      if (response.error) {
-      } else {
-        const token = response.id;
-        const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden">`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
-      }
+
+  const form = document.getElementById('charge-form');
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    try {
+      const { token } = await payjp.createToken(numberElement);
+      const tokenObj = `<input value=${token} name='token' type="hidden">`;
+
+      form.insertAdjacentHTML("beforeend", tokenObj);
+      form.submit();
+    } catch (error) {
+      // エラーハンドリングを追加
+      console.error('トークンの生成に失敗しました:', error.message);
+    } finally {
+      // フォームのクリア処理を追加
       numberElement.clear();
       expiryElement.clear();
       cvcElement.clear();
-      document.getElementById("charge-form").submit();
-    });
-    e.preventDefault();
+    }
   });
 };
 
-window.addEventListener("load", pay);
+// ページが読み込まれたときに pay 関数を実行
+document.addEventListener("DOMContentLoaded", () => {
+  pay();
+});
