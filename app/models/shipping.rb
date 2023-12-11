b8fe37aeb3d5b059eb2f9a4a8c67724764813126
masaki-yosida <yashukasono@gmail.com>
2023-12-11 14:00:05 +0900

    任意のところを空白でも登録可能

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,5 +1,7 @@
 # app/models/shipping.rb
 class Shipping < ApplicationRecord
   belongs_to :purchase
-  validates :post_code, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number, presence: true
+  validates :post_code, :prefecture_id, :municipalities, :street_address, :telephone_number, presence: true
+ 
+
 end
