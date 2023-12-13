5f917304bc90242b0092f4ba646d22b57f1eeca5
masaki-yosida <yashukasono@gmail.com>
2023-12-13 17:16:29 +0900

    編集に飛べないの直した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :set_item, only: [:edit, :update, :show, :destroy]
   before_action :redirect_if_not_item_owner, only: [:edit, :destroy]
-  before_action :redirect_if_sold_out, only: [:edit]
+ 
 
 
 
