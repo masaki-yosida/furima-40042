c2aae1e1729945b4f499639ec089e5f68f846890
masaki-yosida <yashukasono@gmail.com>
2023-12-14 11:30:15 +0900

    消す

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,37 +1,34 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id] || params[:id])
-  
     if user_signed_in? && current_user == @item.user
       redirect_to root_path, alert: '自分が出品した商品の購入ページにはアクセスできません'
       return
     end
-  
+
     if @item.sold_out?
       redirect_to root_path, alert: 'この商品は売り切れています'
       return
     end
-  
+
     @purchase_shipping = PurchaseShipping.new
   end
 
   def new
   end
+
   def create
-    @item = Item.find(params[:item_id])
     @purchase_shipping = PurchaseShipping.new(purchase_params)
-    if 
-      @purchase_shipping.valid?
+    if @purchase_shipping.valid?
       @purchase_shipping.save
       pay_item
       redirect_to root_path
     end
   end
-
   def index
-    @item = Item.find(params[:item_id] || params[:id])
+    before_action :set_item, || params[:id])
   
     if user_signed_in?
       if current_user == @item.user
@@ -52,6 +49,10 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def purchase_params
     params.require(:purchase_shipping).permit(:item_id, :post_code, :prefecture_id, :municipalities, :building_name, :street_address, :building_name, :telephone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
