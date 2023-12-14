c8a52e49d6fcbd3a1d8d4f65ffda3d28fce99f8f
masaki-yosida <yashukasono@gmail.com>
2023-12-14 11:14:17 +0900

    消す

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,57 +1,35 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_item, only: [:index, :create]
 
-  def index
-    @item = Item.find(params[:item_id] || params[:id])
-  
-    if user_signed_in? && current_user == @item.user
-      redirect_to root_path, alert: '自分が出品した商品の購入ページにはアクセスできません'
-      return
-    end
-  
-    if @item.sold_out?
-      redirect_to root_path, alert: 'この商品は売り切れています'
-      return
-    end
-  
-    @purchase_shipping = PurchaseShipping.new
-  end
 
   def new
   end
+
+  def index
+    redirect_if_item_owner
+    @purchase_shipping = PurchaseShipping.new
+  
+    if @item.sold_out?
+      redirect_to_sold_out
+      return  # アクションの実行を終了する
+    end
+  end
+
   def create
-    @item = Item.find(params[:item_id])
     @purchase_shipping = PurchaseShipping.new(purchase_params)
-    if 
-      @purchase_shipping.valid?
+    if @purchase_shipping.valid?
       @purchase_shipping.save
       pay_item
       redirect_to root_path
     end
   end
 
-  def index
-    @item = Item.find(params[:item_id] || params[:id])
-  
-    if user_signed_in?
-      if current_user == @item.user
-        redirect_to root_path, alert: '自分が出品した商品の購入ページにはアクセスできません'
-        return
-      end
-    else
-      redirect_to new_user_session_path, alert: 'ログインしてください'
-      return
-    end
-  
-    if @item.sold_out?
-      redirect_to root_path, alert: 'この商品は売り切れています'
-      return
-    end
-  
-    @purchase_shipping = PurchaseShipping.new
-  end
-
   private
+
+  def set_item
+    @item = Item.find(params[:item_id] || params[:id])
+  end
 
   def purchase_params
     params.require(:purchase_shipping).permit(:item_id, :post_code, :prefecture_id, :municipalities, :building_name, :street_address, :building_name, :telephone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
@@ -64,5 +42,16 @@ class OrdersController < ApplicationController
       card: params[:token],
       currency: 'jpy'
     )
+  end
+
+  def redirect_if_item_owner
+    if user_signed_in? && current_user == @item.user
+      redirect_to root_path, alert: '自分が出品した商品の購入ページにはアクセスできません'
+      return  # アクションの実行を終了する
+    end
+  end
+
+  def redirect_to_sold_out
+    redirect_to root_path, alert: 'この商品は売り切れています'
   end
 end
