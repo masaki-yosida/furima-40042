5f5414d1500c82672396fa2b99deb9546ddd02d8
masaki-yosida <yashukasono@gmail.com>
2023-12-15 12:19:16 +0900

    おけ

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
 
        validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/, message: "is invalid. Include both letters and numbers" }
-       validates_confirmation_of :password, message: "doesn't match Password"
+       
      
 
        validates :firstname_kanji, :lirstname_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
